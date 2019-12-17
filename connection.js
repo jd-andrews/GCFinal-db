59cd2d6d512461779b2af018a0b88164c86a654b
JD <jdandrews357@gmail.com>
2019-12-17 09:55:55 -0500

    heroku deployment

--- connection.js
+++ connection.js
@@ -1,10 +1,24 @@
+// const { Pool } = require("pg");
+// const credentials = new Pool({
+//   user: "postgres",
+//   password: "password", // your password
+//   host: "localhost",
+//   port: 5432,
+//   database: "WouldURather", // select your database
+//   ssl: false
+// });
+// module.exports = credentials;
+
 const { Pool } = require("pg");
-const credentials = new Pool({
-  user: "postgres",
-  password: "password", // your password
-  host: "localhost",
-  port: 5432,
-  database: "WouldURather", // select your database
-  ssl: false
+try {
+  // When not running via Heroku, this will load the .env file.
+  require("dotenv").config();
+} catch (e) {
+  // When running with Heroku, dotenv doesn't need to be available.
+}
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: !connectionString.includes("localhost")
 });
-module.exports = credentials;
+module.exports = pool;
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
-const port = 3003;
-app.listen(port, () => {
-  console.log(`Express started. http://localhost:${port}`);
-});
+
+// define the port
+const DEFAULT_PORT = 3003;
+// Use Heroku's PORT or default to 3000.
+const port = process.env.PORT || DEFAULT_PORT;
+// run the server
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
